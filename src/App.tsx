a4050d29f15f3407c260ed76db297eda4ffe2a12
unmarked4683 <pbuk60@gmail.com>
2024-09-11 07:57:55 +0200

    Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
 import { MainView } from './views/MainView';
 
-export const App = () => {
-	return <MainView />;
-};
+// import { MainView } from './views/MainView';
+
+export const App = () => (
+	<Routes>
+		<Route path='/' element={<MainView />} />
+	</Routes>
+);
