d97ab4e2de6e1c8bcd11ce9d0f1097e48d8d8997
unmarked4683 <pbuk60@gmail.com>
2024-09-12 10:02:03 +0200

    React router added, views to do

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { MainView } from './views/MainView';
+import { ErrorView } from './views/ErrorView';
 
 // import { MainView } from './views/MainView';
 
 export const App = () => (
 	<Routes>
 		<Route path='/' element={<MainView />} />
+		<Route path='/error' element={<ErrorView />} />
 	</Routes>
 );
